d5450a0bd229263261d56bf5bf96d92eaf9cc52b
Park-Sejin <stsj84@gmail.com>
2025-09-15 23:59:49 +0900

    [2025.09.15] 1. 임시 코드

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [boardList, setBoardList] = useState(false);
 
   return (
+    <div className="App">
+      <div className="black-nav">
+        <h4>게시판 만들기</h4>
+      </div>
+
+      <div className="list">
+        <h4>LSB</h4>
+        <p onClick={ ()=> { setBoardList(!boardList) } }>목록 페이지</p>
+      </div>
+      <div className="list">
+        <h4>PSJ</h4>
+        <p onClick={ ()=> { setBoardList(!boardList) } }>목록 페이지</p>
+      </div>
+
+      {
+        boardList? <BoardList></BoardList> : null
+      }
+      
+    </div>
+  )
+}
+
+function BoardList() {
+  return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="modal">
+        <h4>제목</h4>
+        <p>내용</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
